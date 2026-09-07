4d8c2cbd47e1a32492bce2b982c0c4865a2d9f4e
chams1517 <141412873+chams1517@users.noreply.github.com>
2026-09-07 14:33:56 +0200

    Delete repository directory

--- repository/StationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chams.tpe_backend.repository;
-import com.chams.tpe_backend.model.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-public interface StationRepository extends JpaRepository<Station, Long> {
-
-}
